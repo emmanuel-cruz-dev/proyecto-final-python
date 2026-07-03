7e038ef479ab31be96e271352e34003858d6689c
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-07-03 08:42:13 -0300

    feat: adds menu.py

---
+++ menu.py
@@ -0,0 +1,15 @@
+def mostrar_menu():
+    """Muestra el menú principal y gestiona la interacción con el usuario."""
+
+    menu = """
+==================== Menú ====================
+1. Registrar nuevo producto
+2. Mostrar lista de productos
+3. Actualizar producto (por ID)
+4. Eliminar producto (por ID)
+5. Búsqueda de productos (por nombre, categoria o ID)
+6. Reporte de productos (por cantidad)
+7. Salir
+==============================================\n"""
+
+    print(menu)
